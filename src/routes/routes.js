490ee4c0f5a10043b7086417d57cdf786badde62
fabiojr933 <fabiojr933@gmail.com>
2026-02-27 20:29:50 -0400

    -

--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboard = require('../controllers/DashboardController');
-
-router.get('/', dashboard.index);
-router.get('/teste', dashboard.teste);
-
-module.exports = router;
