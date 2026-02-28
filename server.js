fbde3d2262e994394796504b8948f650ae5f4311
fabiojr933 <fabiojr933@gmail.com>
2026-02-27 20:50:17 -0400

    *

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const si = require('systeminformation');
 const axios = require('axios');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3019;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); // Garante o caminho da pasta views
